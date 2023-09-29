0be1af4261685f2d5f1ac91de0aa7e73734b2ff4
Dip Dey <ddeyphysics2010@gmail.com>
2023-09-29 23:31:06 +0530

    navbar

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -2,7 +2,6 @@ import './Navbar.scss'
 import { useAuth } from '../../Utils/AuthContext';
 import { Sling as Hamburger } from 'hamburger-react'
 import { useState } from 'react';
-import { GiLightningHelix } from 'react-icons/gi';
 import {FiLogOut} from 'react-icons/fi';
 import {IoSettings} from 'react-icons/io5';
 import {RiShutDownLine} from 'react-icons/ri'
@@ -10,6 +9,10 @@ import TailSpin from 'react-loading-icons/dist/esm/components/tail-spin'
 import { Link } from 'react-router-dom';
 import {LogoAppleAr} from '@styled-icons/ionicons-solid/LogoAppleAr'
 import {LightningChargeFill} from '@styled-icons/bootstrap/LightningChargeFill'
+import {PersonAccounts} from '@styled-icons/fluentui-system-filled/PersonAccounts'
+import {StatsBars} from '@styled-icons/icomoon/StatsBars'
+import {ClockHistory} from '@styled-icons/bootstrap/ClockHistory'
+import {Wallet2} from '@styled-icons/bootstrap/Wallet2'
 
 const NavbarMenu = () => {
   const {user, handleLogout, buttonSpin, routingStatus, handleRoutingON, handleRoutingOFF} = useAuth()
@@ -33,7 +36,7 @@ const NavbarMenu = () => {
       {user? <button onClick={handleLogout} className='logout-lg'>{buttonSpin? <TailSpin style={{height:'25px'}}/>: <span className='logout-span'><FiLogOut className='logout-icon'/></span>}</button> : null}
       </div>
       <div className='support-settings'>
-        <Link></Link><IoSettings className='settings'/>
+        <Link><IoSettings className='settings'/></Link>
       </div>
       </div>
     </div>
@@ -41,11 +44,10 @@ const NavbarMenu = () => {
     <div className='logo-tablet'>
       <div className='logo'><LogoAppleAr size={30}/><span>Creative Code</span></div>
     </div>
-    <div className='links'>
-        <div className='core-link'><Link to='/profile' className='core-link-ds'>Profile</Link></div>
-        <div className='core-link'><Link to='/stats' className='core-link-ds'>Stats</Link></div>
-        <div className='core-link'><Link to='/history' className='core-link-ds'>History</Link></div>
-        <div className='core-link'><Link to='/wallet' className='core-link-ds'>Wallet</Link></div>
+    <div className='links-tablet'>
+      <Link className='link-tab' to='/profile'><PersonAccounts size={22}/></Link>
+      <Link className='link-tab' to='/stats'><StatsBars size={22}/></Link>
+      <Link className='link-tab' to='/history'><ClockHistory  size={22}/></Link>
     </div>
     <div className='right-toggle'>
       <div className='routing-toggle'>
@@ -55,7 +57,7 @@ const NavbarMenu = () => {
       {user? <button onClick={handleLogout} className='logout-lg'>{buttonSpin? <TailSpin style={{height:'25px'}}/>: <span className='logout-span'><FiLogOut className='logout-icon'/></span>}</button> : null}
       </div>
       <div className='support-settings'>
-        <Link></Link><IoSettings className='settings'/>
+        <Link><IoSettings className='settings'/></Link>
       </div>
       </div>
     </div>
@@ -64,25 +66,27 @@ const NavbarMenu = () => {
       <div className='logo-mobile'>
       <div className='logo'><LogoAppleAr size={30}/><span>Creative Code</span></div>
       </div>
-      <div><Hamburger className='hamburger' color='#19A7CE' size={20} toggled={isOpen} toggle={setOpen}/></div>
+      <div className='right-box'>
+      <div className='routing-toggle'>
+          {routingStatus ? <button className='stop-routing' onClick={(e) => handleRoutingOFF(e,user.$id)}><RiShutDownLine size={14}/></button> : <button className='routing-on' onClick={(e) =>handleRoutingON(e,user.$id)}><LightningChargeFill size={15}/></button>}
+      </div>
+        <Hamburger className='hamburger' color='#19A7CE' size={20} toggled={isOpen} toggle={setOpen}/>
+      
+      </div>
     </div>
     <div className='toggle-menu'>
       <div className={isOpen ? 'menu-container' : 'close-menu-container' }>
         <div className='mob-link'>
-          <div><Link to='/profile' className='core-link-sm'>Profile</Link></div>
-          <div><Link to='/stats' className='core-link-sm'>Statistics</Link></div>
-          <div><Link to='/history' className='core-link-sm'>History</Link></div>
+        <Link className='link-tab' to='/profile'><PersonAccounts size={22}/></Link>
+        <Link className='link-tab' to='/stats'><StatsBars size={22}/></Link>
+        <Link className='link-tab' to='/history'><ClockHistory  size={22}/></Link>
         </div>
-        <div className='mob-wallet-support'>
-          <div><Link to='/history' className='core-link-sm'>Wallet</Link></div>
-          <div><Link to='/history' className='core-link-sm'>Settings</Link></div>
-        </div>
-        <div className='routing-div'>
-          {routingStatus ? <button className='stop-routing' onClick={(e) => handleRoutingOFF(e,user.$id)}>Stop Doubts</button> : <button className='routing-on' onClick={(e) =>handleRoutingON(e,user.$id)}>Start Doubts</button>}
-        </div>
-        <div className='signing-button-div'>
-          {user? <button onClick={handleLogout} className='logout'>{buttonSpin? <TailSpin style={{height:'25px'}}/>: <span>Logout</span>}</button> : null} 
-        </div>
+        <div className='support-settings'>
+        <Link><IoSettings className='settings'/></Link>
+      </div>
+      <div className='logout'>
+      {user? <button onClick={handleLogout} className='logout-lg'>{buttonSpin? <TailSpin style={{height:'25px'}}/>: <span className='logout-span'><FiLogOut className='logout-icon'/></span>}</button> : null}
+      </div>
         </div>
       </div>
     </div>
